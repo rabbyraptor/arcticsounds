14ead5dc45878ec7a5502e866bd7806dc74f6914
Mathias Hammer <>
2019-09-24 08:54:36 +0200

    Added navigation menu component

---
+++ components/navigationMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Line-up</a>
+      </li>
+      <li>
+        <a href="#">Program</a>
+      </li>
+      <li>
+        <a href="#">Who's Behind</a>
+      </li>
+      <li>
+        <a href="#">Tickets</a>
+      </li>
+      <li>
+        <a href="#">Backstage</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+a{
+    color:#7e959c;
+}
+</style>
