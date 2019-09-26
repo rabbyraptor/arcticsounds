139c0afd338e51630285c99d3af77f9b0c9f9027
Mathias Hammer <>
2019-09-26 14:05:34 +0200

    Updated Instagram-feed
    Updated Instagram access token
    Updated instagram-feed styling

--- components/instagramSection.vue
+++ components/instagramSection.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="instagram-section">
     <div class="ig-logo">
-        <h2>Arctic Sounds On</h2>
-      <img src="/img/instagram-logo.png" />
+      <h2>Arctic Sounds On</h2>
+      <a href="https://www.instagram.com/arctic_sounds/" target="_blank">
+        <img src="/img/instagram-logo.png" />
+      </a>
     </div>
     <vue-instagram
-      token="37850897.1677ed0.e07d1c8b410842af8c53d52012e57a90"
+      token="7360104530.1677ed0.c8118f00548644349a14969a773d7e0f"
       :count="9"
-      mediaType="image"
+      mediaType="image, video"
       class="ig-grid"
     >
       <template slot="feeds" slot-scope="props">
-          <li class="fancy-list ig-img">
-            <img :src="props.feed.images.low_resolution.url" />
-          </li>
+        <a :href="props.feed.link" target="_blank" class="ig-img">
+          <img :src="props.feed.images.low_resolution.url" />
+        </a>
       </template>
-      <template slot="error" slot-scope="props">
+      <!--       <template slot="error" slot-scope="props">
         <div class="fancy-alert">{{ props.error.error_message }}</div>
-      </template>
+      </template>-->
     </vue-instagram>
   </div>
 </template>
