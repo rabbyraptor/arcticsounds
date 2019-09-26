5bffc2fd93597c21641fb067f78605147b0467d3
Mathias Hammer <>
2019-09-26 08:40:11 +0200

    Contact info, fonts

---
+++ components/contactInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <slot></slot>
+    <li v-for="info in contactInfo" :key="info.name">
+      <span class="icon" v-html="info.icon" /><span>{{ info.info }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contactInfo: [
+        {
+          name: "Phone",
+          icon: "&#xe090;",
+          info: "(+299) 48 69 50"
+        },
+        {
+          name: "Email",
+          icon: "&#xe076;",
+          info: "info@arcticsounds.gl"
+        },
+        {
+          name: "Address",
+          icon: "&#xe074;",
+          info: "Sisimiut, Greenland",
+        }
+      ]
+    };
+  }
+};
+</script>
