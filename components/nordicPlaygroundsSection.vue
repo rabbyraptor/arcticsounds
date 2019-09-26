68103cb1c020bb41689cb36d092b11c9defb801a
Mathias Hammer <>
2019-09-26 16:39:33 +0200

    Who's behind page + Nordic playgrounds section added

---
+++ components/nordicPlaygroundsSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="nordic-playgrounds-section">
+    <h1>Who's Behind</h1>
+    <h2>Nordic Playgrounds</h2>
+    <p>Nordic Playgrounds is a collection of activities that strive to balance a playful approach to serious collaboration with targeted social events and network meetings.</p>
+    <br />
+    <p>In 2020 we feature playgrounds and cocreation activities in these fields:</p>
+    <ul>
+      <li>Network development</li>
+      <li>Recording and music production</li>
+      <li>Electronic music</li>
+      <li>Music and the media</li>
+      <li>Songwriting / co-writing</li>
+      <li>Performance</li>
+      <li>Fundraising</li>
+      <li>Event management</li>
+      <li>a.o.</li>
+    </ul>
+    <br />
+    <p>
+      There will also be excellent opportunities for getting out in the back country by
+      <strong>snowmobile</strong> or
+      <strong>dogsled</strong> and experience the extraordinary greenlandic nature firsthand.
+    </p>
+    <br />
+    <p>
+      There’s also still a few open spots on
+      <a
+        href="http://www.listeningroomretreats.com/current-retreats/sisimiut-greenland"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Greenland Listening Room Retreat</a> if you want to immerse yourself fully in the arts and craft of songwriting with fellow songwriters for 5 days.
+    </p>
+    <br />
+    <p>Dates are: April 4th – April 9nd</p>
+    <p>Come join us for an unforgettable and productive time!</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/whosbehind.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section>
+      <nordic-playgrounds-section />
+    </section>
+  </div>
+</template>
+
+<script>
+import nordicPlaygroundsSection from '../components/nordicPlaygroundsSection'
+
+export default {
+  components: {
+    nordicPlaygroundsSection
+  }
+};
+</script>
