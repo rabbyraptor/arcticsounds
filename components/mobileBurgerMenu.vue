e56b2992c37fa904fdfa28e59161f51b36268181
Mathias Hammer <>
2019-10-03 16:31:42 +0200

    Added mobile burger menu

---
+++ components/mobileBurgerMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="burger-menu-icon-wrapper" @click="toggleBurgerMenu">
+    <div id="burger-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    /* bmiw.onclick = function() {
+      bm.classList.toggle("open");
+      if (bm.classList.contains("open")) {
+        mobile.style.display = "grid";
+        setTimeout(function() {
+          mobile.style.opacity = 1;
+        }, 1);
+      } else if (!bm.classList.contains("open")) {
+        mobile.style.opacity = 0;
+        setTimeout(function() {
+          mobile.style.display = "none";
+        }, 250);
+      }
+    }; */
+  },
+  methods: {
+    toggleBurgerMenu() {
+      const bm = document.getElementById("burger-menu"),
+        mobile = document.getElementsByClassName("mobile")[0];
+      bm.classList.toggle("open");
+      if (bm.classList.contains("open")) {
+        mobile.style.display = "grid";
+        setTimeout(function() {
+          mobile.style.opacity = 1;
+        }, 1);
+      } else if (!bm.classList.contains("open")) {
+        mobile.style.opacity = 0;
+        setTimeout(function() {
+          mobile.style.display = "none";
+        }, 250);
+      }
+    }
+  }
+};
+</script>
