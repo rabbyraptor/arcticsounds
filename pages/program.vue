c2be289239e0e5245c6deb656876105533aa7e32
Mathias Hammer <>
2019-10-08 16:31:31 +0200

    Update program.vue

--- pages/program.vue
+++ pages/program.vue
@@ -1,6 +1,20 @@
 <template>
-    <section class="full-width-section">
-      <golive-section :datatype="datatype" />
+    <section>
+      <!-- 
+      <goevent-web
+      id="goevent-web"
+      project-tag="arcticsoundfestival-2019"
+      :project-hash="goeventHash"
+      data-language="eng"
+      data-default-url="artists"
+    ></goevent-web>
+    <script
+      id="gw-script"
+      type="text/javascript"
+      src="https://s3.amazonaws.com/goeventweb-static.greencopper.com/7.7.0/public/scripts/endpoint.min.gz.js"
+      data-gzip="true"
+    ></script> -->
+      <!-- <golive-section :datatype="datatype" /> -->
     </section>
 </template>
 
@@ -10,7 +24,8 @@ import goliveSection from '../components/goliveSection'
 export default {
   data(){
     return{
-      datatype:'schedule',
+      datatype:'artists',
+      goeventHash: process.env.GREENCOPPER_GOEVENT_HASH
     }
   },
   components: {
