5ddd447abcf9f08b79ff7f9026b6dcd07cb36cbc
Mathias Hammer <>
2019-10-25 10:50:43 +0200

    Update programSection.vue
    - Updated dates from strings to Date()
    - Updated past 00 o' clock playtimes to be sorted last
    - Added month-array, so that months are no longer hard coded
    - Install lodash

--- components/programSection.vue
+++ components/programSection.vue
@@ -2,18 +2,23 @@
   <div class="program-section">
     <h1>Program</h1>
     <div class="schedule">
-      <div class="day" v-for="day in dates" :key="day">
+      <div class="day" v-for="day in sortedDates" :key="day">
         <div class="weekday">
-          <h2>{{ getWeekday(day) }}</h2>
+          <h2>{{ getWeekday(day) }} {{ day }}</h2>
         </div>
-        <div class="row" v-for="venue in venues" :key="venue" v-show="showsFromVenue(venue, day).length > 0">
+        <div
+          class="row"
+          v-for="venue in venues"
+          :key="venue"
+          v-if="showsFromVenue(venue, day).length > 0"
+        >
           <div class="venue">
             <h4 class="venue-title">{{ venue }}</h4>
           </div>
           <div class="shows">
             <div class="show" v-for="show in showsFromVenue(venue, day)">
               <h5 class="show-time">
-                <span>{{ show.start_time }}</span>
+                <span>{{ get24hTime(show.start_time) }}</span>
                 <span v-if="show.end_time">– {{ show.end_time.substr(0, 5) }}</span>
               </h5>
               <h5>{{ show.title }}</h5>
@@ -26,6 +31,7 @@
 </template>
 
 <script>
+var _ = require("lodash");
 export default {
   data() {
     return {
@@ -38,16 +44,32 @@ export default {
       for (let i in this.program) {
         if (
           this.program[i].venue.title == venue &&
-          this.program[i].date_start == day
-        )
-          shows.push({
-            title: this.program[i].object.title,
-            start_time: this.program[i].time_start.substr(0, 5),
-            end_time: this.program[i].time_end
-          });
-      }      
+          new Date(this.program[i].date_start).getDate() == new Date(day).getDate()
+        ) {
+          if (parseInt(this.program[i].time_start.substr(0, 5)) <= 4) {
+            shows.push({
+              title: this.program[i].object.title,
+              start_time: "24" + this.program[i].time_start.substr(2, 3),
+              end_time: this.program[i].time_end
+            });
+          } else {
+            shows.push({
+              title: this.program[i].object.title,
+              start_time: this.program[i].time_start.substr(0, 5),
+              end_time: this.program[i].time_end
+            });
+          }
+        }
+      }
       shows.sort((a, b) => parseInt(a.start_time) - parseInt(b.start_time));
       return shows;
+    },
+    get24hTime(time) {
+      if (parseInt(time) > 23) {
+        return "00" + time.substr(2, 5);
+      } else {
+        return time;
+      }
     },
     getWeekday(day) {
       let d = new Date(day);
@@ -60,7 +82,24 @@ export default {
         "Friday",
         "Saturday"
       ];
-      return days[d.getDay()] + " April " + day.substr(8, 2);
+      return days[d.getDay()];
+    },
+    getMonthName(month) {
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      return months[month.getMonth()];
     }
   },
   computed: {
@@ -73,18 +112,25 @@ export default {
     venues() {
       let venues = [];
       for (let i in this.program) {
-        if (!venues.includes(this.program[i].venue.title))
           venues.push(this.program[i].venue.title);
       }
-      return venues;
+      return _.uniqWith(venues, _.isEqual);
     },
-    dates() {
+    fullDates() {
       let dates = [];
       for (let i in this.program) {
-        if (!dates.includes(this.program[i].date_start))
-          dates.push(this.program[i].date_start);
+        dates.push(new Date(this.program[i].date_start + "Z"));
       }
-      return dates.sort();
+      return _.uniqWith(dates, _.isEqual);
+    },
+    sortedDates() {
+      let sortedDates = [];
+      for (let i in this.fullDates) {
+        sortedDates.push(
+          this.fullDates[i].getDate() + " " + this.getMonthName(this.fullDates[i])
+        );
+      }
+      return sortedDates.sort();
     }
   }
 };
