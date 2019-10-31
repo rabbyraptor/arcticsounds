d0fc8238914c617ffa8171eb4fa3f21f90f7550c
Mathias Hammer <>
2019-10-31 16:27:05 +0100

    Add artist-tag component

---
+++ components/artistTag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="artist-tag" :style="{color: tag.color, opacity: this.opacity}">
+    <span class="tag-color" v-if="tag.color">&#9679;</span>
+    <span class="tag-name">{{ tag.name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tag:{},
+    active:{}
+  },
+  data(){
+    return{
+      opacity:{}
+    }
+  },
+  watch:{
+    active(){
+      if(this.active == this.tag.id){
+        this.opacity = 1;
+      }
+      else if (this.active == this.tag.id && this.opacity == 1){
+        this.opacity = 0;
+      }
+      else {
+        this.opacity = .3;
+      }
+    }
+  }
+};
+</script>
