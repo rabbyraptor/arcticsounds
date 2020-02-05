6ec4265b9d9eb6e92bf8c052890efe292bf241da
Mathias Hammer <>
2020-02-05 18:22:26 +0100

    Create tickets.vue

---
+++ pages/tickets.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <section class="full-width-section no-top-padding">
+      <ticket-integration />
+    </section>
+  </div>
+</template>
+
+<script>
+import ticketIntegration from "~/components/ticketIntegration.vue";
+
+export default {
+  components: {
+    ticketIntegration
+  },
+};
+</script>
+
+<style>
+</style>
